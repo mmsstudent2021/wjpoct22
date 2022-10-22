89823e9cd757b756f8d0b5c6abc0d606c98f8d15
mmsstudent2021 <mmsstudent2021@gmail.com>
2022-10-22 10:18:52 +0630

    todel

--- js/piano.js
+++ js/piano.js
@@ -14,15 +14,18 @@ sounds.forEach(sound => {
     const keyBtn = document.createElement('div');
     keyBtn.classList.add('col');
     keyBtn.innerHTML = `<div key="${sound}" class="key bg-primary text-center fs-4 text-white rounded d-flex align-items-end justify-content-center"><p class="pe-none">${sound}</p></div>`;
-
     keybord.append(keyBtn)
+    const audio = new Audio(getSoundUrl(sound));
+    audio.controls = true
+    document.body.append(audio)
 })
 
 
 document.querySelectorAll('.key').forEach(key => {
     key.addEventListener('click',_=> {
         const currentPressKey = key.getAttribute('key');
-        const sound = new Audio(getSoundUrl(currentPressKey));
-        sound.play()
+        const currentAudio = document.querySelector(`[src*='${currentPressKey}.mp3']`)
+        console.log(currentAudio)
+        currentAudio.play()
     })
 })
